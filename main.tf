a78b65cb776192b986bd3363004ea7ffee19f8b6
Suheib Marzouka <suheibmarzouka@Mac.fritz.box>
2024-11-15 10:23:27 +0100

    add public Key to ec2 machines

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "GithubActionsInstanz" {
   count = 5  
   ami           = "ami-0eddb4a4e7d846d6f"
   instance_type = "t2.micro"
+  key_name = "terraformKey"
   tags = {
     Name = "Meine Github Actions Instanz ${count.index}"
   }
